101fa8523c918907cc702c82baf83dc63cac1361
nickedwards109 <nicholas.lee.edwards@gmail.com>
2018-12-02 16:33:32 -0500

    Display the current UTC time
    This commit applies changes that display the current UTC time in the HTML at the time the page is loaded.

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import time
 
 app = Flask('isItElevenEleven')
 
 @app.route("/")
-def hello_world():
-	html = "<div>Hello World! The application server has served up some HTML.</div>"
+def display_the_time():
+	utc_time = time.gmtime()
+
+	seconds_into_current_minute = utc_time.tm_sec
+	minutes_into_current_hour = utc_time.tm_min
+	hours_into_current_day = utc_time.tm_hour
+
+	html = "<div>The current UTC time is {hour}:{minute}:{second}</div>" \
+	.format(hour=hours_into_current_day, minute=minutes_into_current_hour, second=seconds_into_current_minute)
+
 	return html
 
 app.run(host='0.0.0.0', port=80)
