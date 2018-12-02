ffd65508a497b7e2d9467d28c0b5f1f916480449
nickedwards109 <nicholas.lee.edwards@gmail.com>
2018-12-02 16:47:21 -0500

    Display whether the time is 11:11
    This commit adds some logic that figures out whether the time is 11:11 or not and then displays the result in the HTML.

--- server.py
+++ server.py
@@ -4,15 +4,20 @@ import time
 app = Flask('isItElevenEleven')
 
 @app.route("/")
-def display_the_time():
+def is_it_eleven_eleven():
 	utc_time = time.gmtime()
 
-	seconds_into_current_minute = utc_time.tm_sec
 	minutes_into_current_hour = utc_time.tm_min
 	hours_into_current_day = utc_time.tm_hour
 
-	html = "<div>The current UTC time is {hour}:{minute}:{second}</div>" \
-	.format(hour=hours_into_current_day, minute=minutes_into_current_hour, second=seconds_into_current_minute)
+	minutes_is_eleven = True if minutes_into_current_hour == 11 else False
+	hours_is_eleven = True if hours_into_current_day == 11 or hours_into_current_day == 23 else False
+	is_it_eleven_eleven = True if minutes_is_eleven and hours_is_eleven else False
+	is_it_eleven_eleven_display_text = "Yes" if is_it_eleven_eleven == True else "No"
+
+	html = "<div>The current UTC time is {hour}:{minute}</div>" \
+	"<div>Is it 11:11? {yes_or_no}.</div>" \
+	.format(hour=hours_into_current_day, minute=minutes_into_current_hour, yes_or_no=is_it_eleven_eleven_display_text)
 
 	return html
 
